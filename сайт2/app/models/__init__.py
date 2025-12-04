f2a4b2763b4e33c107966bc84558160b4b358cb0
DanilchenkoDima <bravlgopatyc@gmail.com>
2025-12-04 17:39:34 +0300

    Delete сайт2 directory

--- сайт2/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .roles import RoleModel
-from .users import UserModel
-from .rooms import RoomModel
-from .booking import BookingModel
-
-__all__ = ["RoleModel", "UserModel", "RoomModel", "BookingModel"]
