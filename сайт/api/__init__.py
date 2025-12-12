b1e3406c04a1395c443735ec07210dc0db26efcd
DanilchenkoDima <bravlgopatyc@gmail.com>
2025-12-13 02:10:12 +0300

    Delete сайт directory

--- сайт/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from api.users import users_router
-from api.rooms import rooms_router
-from api.bookings import bookings_router
-from api.admin import admin_router
-from api.roles import roles_router
-
-__all__ = ['users_router', 'rooms_router', 'bookings_router', 'admin_router', 'roles_router']
