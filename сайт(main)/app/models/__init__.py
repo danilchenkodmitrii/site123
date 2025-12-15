558442916e6d5c1bd5d2d90629f903c214a87d3e
DanilchenkoDima <bravlgopatyc@gmail.com>
2025-12-16 00:40:30 +0300

    Delete сайт(main) directory

--- сайт(main)/app/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .models import User, Room, Booking, Role, Base, async_session, engine, init_db, get_db
-
-__all__ = ['User', 'Room', 'Booking', 'Role', 'Base', 'async_session', 'engine', 'init_db', 'get_db']
--- сайт(main)/app/services/init.py
+++
@@ -1,6 +0,0 @@
-from .user_service import UserService
-from .room_service import RoomService
-from .booking_service import BookingService
-from .role_service import RoleService
-
-__all__ = ['UserService', 'RoomService', 'BookingService', 'RoleService']
