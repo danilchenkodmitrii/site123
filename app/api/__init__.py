47e5afe7ef7f043f35b4d9c10fd824c57a0a117b
DanilchenkoDima <bravlgopatyc@gmail.com>
2025-12-16 18:43:16 +0300

    Delete app directory

--- app/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from .users import users_router
-from .rooms import rooms_router
-from .bookings import bookings_router
-from .admin import admin_router
-from .roles import roles_router
-
-__all__ = ['users_router', 'rooms_router', 'bookings_router', 'admin_router', 'roles_router']
--- app/repositories/__init__.py
+++
@@ -1,6 +0,0 @@
-from .user_repository import UserRepository
-from .room_repository import RoomRepository
-from .booking_repository import BookingRepository
-from .role_repository import RoleRepository
-
-__all__ = ['UserRepository', 'RoomRepository', 'BookingRepository', 'RoleRepository']
